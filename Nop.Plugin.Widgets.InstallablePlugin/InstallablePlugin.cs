2b1443151bcd2d30df438b2431a2454379e8e1ee
dash-web <adonayg@outlook.com>
2020-10-18 14:57:45 +0100

    Installable Plugin

---
+++ Nop.Plugin.Widgets.InstallablePlugin/InstallablePlugin.cs
@@ -0,0 +1,18 @@
+﻿
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Widgets.InstallablePlugin
+{
+    public class InstallablePlugin : BasePlugin
+    {
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+    }
+}
