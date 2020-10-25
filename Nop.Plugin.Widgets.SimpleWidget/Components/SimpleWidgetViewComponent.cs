d4c2e2b3af555796dd989980765239e8ea387e61
adonayg3 <adonayg3@outlook.com>
2020-10-25 15:01:44 +0000

    Simple Widget

---
+++ Nop.Plugin.Widgets.SimpleWidget/Components/SimpleWidgetViewComponent.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widgets.SimpleWidget.Components
+{
+    public class SimpleWidgetViewComponent : NopViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Plugins/Widgets.SimpleWidget/Views/SimpleWidgetView.cshtml");
+        }
+    }
+}
---
+++ Nop.Plugin.Widgets.SimpleWidget/SimpleWidget.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Widgets.SimpleWidget
+{
+    public class SimpleWidget : BasePlugin, IWidgetPlugin
+    {
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+
+        public bool HideInWidgetList => false;
+        public IList<string> GetWidgetZones()
+        {
+            return new List<string>{PublicWidgetZones.HomepageBeforeBestSellers};
+        }
+
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            return "SimpleWidget";
+        }
+    }
+}
