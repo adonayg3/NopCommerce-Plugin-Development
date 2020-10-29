538038d93d3be0a0a02825196e49aeaf82e6db62
adonayg3 <adonayg3@outlook.com>
2020-10-29 12:23:04 +0000

    Added view component name

--- Nop.Plugin.Widgets.SimpleWidget/Components/SimpleWidgetViewComponent.cs
+++ Nop.Plugin.Widgets.SimpleWidget/Components/SimpleWidgetViewComponent.cs
@@ -3,6 +3,7 @@ using Nop.Web.Framework.Components;
 
 namespace Nop.Plugin.Widgets.SimpleWidget.Components
 {
+    [ViewComponent(Name = "SimpleWidget")]
     public class SimpleWidgetViewComponent : NopViewComponent
     {
         public IViewComponentResult Invoke()
