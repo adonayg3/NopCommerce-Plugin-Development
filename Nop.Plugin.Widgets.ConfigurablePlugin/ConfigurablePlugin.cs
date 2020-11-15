6edd317954414aa19a7165bab6671f17cfaad119
adonayg3 <adonayg3@outlook.com>
2020-11-15 15:47:40 +0000

    Added Configurable Plugin to Admin menu

--- Nop.Plugin.Widgets.ConfigurablePlugin/ConfigurablePlugin.cs
+++ Nop.Plugin.Widgets.ConfigurablePlugin/ConfigurablePlugin.cs
@@ -1,26 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Routing;
 using Nop.Core;
 using Nop.Services.Cms;
 using Nop.Services.Configuration;
 using Nop.Services.Localization;
 using Nop.Services.Plugins;
+using Nop.Services.Security;
 using Nop.Web.Framework.Infrastructure;
+using Nop.Web.Framework.Menu;
 
 namespace Nop.Plugin.Widgets.ConfigurablePlugin
 {
-    public class ConfigurablePlugin : BasePlugin, IWidgetPlugin
+    public class ConfigurablePlugin : BasePlugin, IWidgetPlugin, IAdminMenuPlugin
     {
         private readonly ILocalizationService _localizationService;
         private readonly ISettingService _settingService;
         private readonly IWebHelper _webHelper;
+        private readonly IPermissionService _permissionService;
 
         public ConfigurablePlugin(ILocalizationService localizationService,
             ISettingService settingService,
-            IWebHelper webHelper)
+            IWebHelper webHelper,
+            IPermissionService permissionService)
         {
             _localizationService = localizationService;
             _settingService = settingService;
             _webHelper = webHelper;
+            _permissionService = permissionService;
         }
         
         public IList<string> GetWidgetZones()
@@ -61,7 +68,49 @@ namespace Nop.Plugin.Widgets.ConfigurablePlugin
 
             base.Uninstall();
         }
-        
+        public void ManageSiteMap(SiteMapNode rootNode)
+        {
+            if (_permissionService.Authorize(StandardPermissionProvider.ManageWidgets))
+            {
+                var menuItem = new SiteMapNode()
+                {
+                    Title = "ConfigurablePlugin",
+                    Visible = true,
+                    IconClass = "fa fa-cogs",
+                    RouteValues = new RouteValueDictionary() {{"area", null}},
+                };
+                
+                var settingsItem = new SiteMapNode()
+                {
+                    Title = "Configure",
+                    ControllerName = "WidgetsConfigurablePlugin",
+                    ActionName = "Configure",
+                    Visible = true,
+                    IconClass = "fa-dot-circle-o",
+                    RouteValues = new RouteValueDictionary() { { "area", "admin" } },
+                    SystemName = "ConfigurablePlugin.Configure"
+                };
+                menuItem.ChildNodes.Add(settingsItem);
+
+                var pluginNode = rootNode.ChildNodes.FirstOrDefault(x => x.SystemName == "CustomPlugins");
+
+                if (pluginNode != null)
+                    pluginNode.ChildNodes.Add(menuItem);
+                else
+                {
+                    var customPluginNode = new SiteMapNode()
+                    {
+                        Visible = true,
+                        Title = "CustomPlugins",
+                        Url = "",
+                        SystemName = "CustomPlugins",
+                        IconClass = "fa fas fa-plug"
+                    };
+                    rootNode.ChildNodes.Add(customPluginNode);
+                    customPluginNode.ChildNodes.Add(menuItem);
+                }
+            }
+        }
         public bool HideInWidgetList => false;
     }
 }
